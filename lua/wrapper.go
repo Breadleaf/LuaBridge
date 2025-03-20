8267d7b993601c5d467c6393391a7aefe1396c6c
Breadleaf <bkhnapa@gmail.com>
2025-03-20 10:27:36 -0600

    added wrappers to expose some lua api, made an example in main.go

--- lua/wrapper.go
+++ lua/wrapper.go
@@ -12,6 +12,326 @@ package lua
 #include "lua.h"
 #include "lualib.h"
 #include "lauxlib.h"
+
+// Macro wrappers:
+
+// The macro
+//   #define lua_tonumber(L,i)	lua_tonumberx(L,(i),NULL)
+// is wrapped as:
+static lua_Number LuaMacro_tonumber(lua_State *L, int index) {
+    return lua_tonumber(L, index);
+}
+
+// The macro
+//   #define lua_tostring(L,i)	lua_tolstring(L, (i), NULL)
+// is wrapped as:
+static const char* LuaMacro_tostring(lua_State *L, int index) {
+    return lua_tostring(L, index);
+}
+
+// The macro
+//   #define lua_pop(L,n)		lua_settop(L, -(n)-1)
+// is wrapped as:
+static void LuaMacro_pop(lua_State *L, int n) {
+    lua_pop(L, n);
+}
+
+// The macro
+//   #define lua_insert(L,idx)	lua_rotate(L, (idx), 1)
+// is wrapped as:
+static void LuaMacro_insert(lua_State *L, int idx) {
+    lua_insert(L, idx);
+}
+
+// The macro
+//   #define lua_remove(L,idx)	(lua_rotate(L, (idx), -1), lua_pop(L, 1))
+// is wrapped as:
+static void LuaMacro_remove(lua_State *L, int idx) {
+    lua_remove(L, idx);
+}
+
+// The macro
+//   #define lua_pushliteral(L, s)	lua_pushstring(L, "" s)
+// is wrapped as:
+static void LuaMacro_pushliteral(lua_State *L, const char* s) {
+    lua_pushstring(L, s);
+}
+
+// The macro
+//   #define lua_pcall(L,n,r,f)	lua_pcallk(L, (n), (r), (f), 0, NULL)
+// is wrapped as:
+static int LuaMacro_pcall(lua_State *L, int n, int r, int f) {
+    return lua_pcallk(L, n, r, f, 0, NULL);
+}
+
+// The macro
+//   #define lua_isnil(L,n)		(lua_type(L, (n)) == LUA_TNIL)
+// is wrapped as:
+static int LuaMacro_isnil(lua_State *L, int n) {
+    return lua_type(L, n) == LUA_TNIL;
+}
+
+// Extern functions:
+
+extern int goLuaCallback(lua_State *L);
+extern int traceback(lua_State *L);
 */
 import "C"
+import (
+	"fmt"
+	"unsafe"
+)
 
+type LuaFunction func(L *LuaState) int
+
+type LuaState struct {
+    L *C.lua_State
+    closures map[int64]LuaFunction
+    closureID int64
+}
+
+var luaStates = make(map[*C.lua_State]*LuaState)
+
+func NewState() *LuaState {
+    L := C.luaL_newstate()
+    if L == nil {
+        panic("failed to create Lua state")
+    }
+
+    // open all standard libraries
+    C.luaL_openlibs(L)
+
+    s := &LuaState{
+        L: L,
+        closures: make(map[int64]LuaFunction),
+        closureID: 0,
+    }
+    luaStates[L] = s
+
+    return s
+}
+
+//export goLuaCallback
+func  goLuaCallback(L *C.lua_State) C.int {
+    state, ok := luaStates[L]
+    if !ok {
+        panic("failed to find Lua state for callback")
+    }
+
+    // get the closure ID from the upvalue
+    id := int64(C.LuaMacro_tonumber(L, 1))
+    if goFunction, found := state.closures[id]; found {
+        return C.int(goFunction(state))
+    }
+
+    return 0
+}
+
+// //export traceback
+// func traceback(L *C.lua_State) C.int {
+//     msg := C.LuaMacro_tostring(L, 1)
+//     if msg != nil {
+//         C.luaL_traceback(L, L, msg, 1)
+//     } else {
+//         C.LuaMacro_pushliteral(L, C.CString("no error message"))
+//     }
+//     return 1
+// }
+
+//export traceback
+func traceback(L *C.lua_State) C.int {
+    errorMsg := C.LuaMacro_tostring(L, 1)
+    if errorMsg == nil {
+        C.LuaMacro_pushliteral(L, C.CString("(no error message)"))
+        return 1
+    }
+    
+    // Get the current Lua state from the registry
+    C.lua_pushthread(L)
+    C.lua_rawget(L, C.LUA_REGISTRYINDEX)
+    
+    // Generate traceback
+    C.luaL_traceback(L, L, errorMsg, 1)
+    return 1
+}
+
+func (s *LuaState) RegisterGlobalFunction(name string, fn LuaFunction) {
+    // save the function in the registry
+    id := s.closureID
+    s.closures[id] = fn
+    s.closureID++
+
+    // push the closure ID as an upvalue
+    C.lua_pushnumber(s.L, C.lua_Number(id))
+    C.lua_pushcclosure(s.L, (*[0]byte)(C.goLuaCallback), 1)
+
+    // set it as a global variable
+    cname := C.CString(name)
+    defer C.free(unsafe.Pointer(cname))
+    C.lua_setglobal(s.L, cname)
+}
+
+// Can be used to remove libraries or other global variables
+func (s *LuaState) RemoveGlobal(name string) {
+    // set the global variable to nil
+    cname := C.CString(name)
+    defer C.free(unsafe.Pointer(cname))
+    C.lua_pushnil(s.L)
+    C.lua_setglobal(s.L, cname)
+}
+
+func (s *LuaState) RunStringError(script string) error {
+    cscript := C.CString(script)
+    defer C.free(unsafe.Pointer(cscript))
+    if C.luaL_loadstring(s.L, cscript) != 0 {
+        err := C.GoString(C.lua_tolstring(s.L, -1, nil))
+        return fmt.Errorf("lua error (load): %s", err)
+    }
+    if C.lua_pcallk(s.L, 0, C.LUA_MULTRET, 0, 0, nil) != 0 {
+        err := C.GoString(C.lua_tolstring(s.L, -1, nil))
+        return fmt.Errorf("lua error (call): %s", err)
+    }
+    return nil
+}
+
+// func (s *LuaState) RunStringTraceback(script string) error {
+//     cscript := C.CString(script)
+//     defer C.free(unsafe.Pointer(cscript))
+
+//     // 1. Load the chunk first.
+//     if C.luaL_loadstring(s.L, cscript) != 0 {
+//         err := C.GoString(C.lua_tolstring(s.L, -1, nil))
+//         C.LuaMacro_pop(s.L, 1) // remove error message from stack
+//         return fmt.Errorf("lua error (load): %s", err)
+//     }
+
+//     // 2. Push the error handler (our C traceback function) onto the stack.
+//     C.lua_pushcclosure(s.L, (*[0]byte)(C.traceback), 0)
+    
+//     // 3. Insert the error handler below the loaded chunk.
+//     // After insertion: error handler is at index -2, chunk at index -1.
+//     C.LuaMacro_insert(s.L, -2)
+    
+//     // 4. Save the error handler's absolute index (it's now at top - 1).
+//     errorHandlerIndex := C.lua_gettop(s.L) - 1
+
+//     // 5. Call the chunk with the error handler in place.
+//     if C.lua_pcallk(s.L, 0, C.LUA_MULTRET, errorHandlerIndex, 0, nil) != 0 {
+//         err := C.GoString(C.lua_tolstring(s.L, -1, nil))
+//         // Remove both the error message and the error handler.
+//         C.lua_settop(s.L, errorHandlerIndex-1)
+//         return fmt.Errorf("lua error (call): %s", err)
+//     }
+
+//     // 6. Clean up: remove the error handler from the stack.
+//     C.LuaMacro_remove(s.L, errorHandlerIndex)
+//     return nil
+// }
+
+func (s *LuaState) RunStringTraceback(script string) error {
+    cscript := C.CString(script)
+    defer C.free(unsafe.Pointer(cscript))
+
+    // Create a message handler for our traceback
+    if err := s.LoadString("local dbg = debug; return function(err) local tb = dbg.traceback(err, 2); return tb; end"); err != nil {
+        return fmt.Errorf("error creating traceback handler: %s", err)
+    }
+    
+    // Handler is now at the top of the stack (-1)
+    errorHandlerIndex := C.lua_gettop(s.L)
+    
+    // Load the script
+    if C.luaL_loadstring(s.L, cscript) != 0 {
+        err := C.GoString(C.lua_tolstring(s.L, -1, nil))
+        C.lua_settop(s.L, errorHandlerIndex-1) // Remove error and handler
+        return fmt.Errorf("lua error (load): %s", err)
+    }
+    
+    // Script is now at the top (-1), handler at -2
+    // Call with the error handler in place
+    if C.lua_pcallk(s.L, 0, C.LUA_MULTRET, errorHandlerIndex, 0, nil) != 0 {
+        // Get the error message with traceback from handler
+        err := C.GoString(C.lua_tolstring(s.L, -1, nil))
+        C.lua_settop(s.L, errorHandlerIndex-1) // Remove error and handler
+        return fmt.Errorf("lua error (call):\n%s", err)
+    }
+    
+    // Remove the handler
+    C.LuaMacro_remove(s.L, errorHandlerIndex)
+    return nil
+}
+
+func (s *LuaState) Close() {
+    C.lua_close(s.L)
+    delete(luaStates, s.L)
+}
+
+// Direct wrappers
+
+func (s *LuaState) ToString(index int) string {
+    return C.GoString(C.LuaMacro_tostring(s.L, C.int(index)))
+}
+
+func (s *LuaState) ToNumber(index int) float64 {
+    return float64(C.LuaMacro_tonumber(s.L, C.int(index)))
+}
+
+func (s *LuaState) PushString(val string) {
+    cval := C.CString(val)
+    defer C.free(unsafe.Pointer(cval))
+    C.lua_pushstring(s.L, cval)
+}
+
+func (s *LuaState) PushNumber(val float64) {
+	C.lua_pushnumber(s.L, C.lua_Number(val))
+}
+
+func (s *LuaState) Pop(n int) {
+	// Set the top of the stack to -n-1.
+	C.lua_settop(s.L, C.int(-n-1))
+}
+
+func (s *LuaState) Insert(index int) {
+	C.LuaMacro_insert(s.L, C.int(index))
+}
+
+func (s *LuaState) Remove(index int) {
+	C.LuaMacro_remove(s.L, C.int(index))
+}
+
+func (s *LuaState) LoadString(script string) error {
+	cscript := C.CString(script)
+	defer C.free(unsafe.Pointer(cscript))
+	if C.luaL_loadstring(s.L, cscript) != 0 {
+		errMsg := s.ToString(-1)
+		// Remove the error message from the stack.
+		s.Pop(1)
+		return fmt.Errorf("lua load error: %s", errMsg)
+	}
+	return nil
+}
+
+func (s *LuaState) PCall(nArgs, nResults, errorFuncIndex int) error {
+	if C.lua_pcallk(s.L, C.int(nArgs), C.int(nResults), C.int(errorFuncIndex), 0, nil) != 0 {
+		errMsg := s.ToString(-1)
+		s.Pop(1) // remove error message
+		return fmt.Errorf("lua error: %s", errMsg)
+	}
+	return nil
+}
+
+func (s *LuaState) IsNil(index int) bool {
+    return C.LuaMacro_isnil(s.L, C.int(index)) != 0
+}
+
+func (s *LuaState) PushBoolean(b bool) {
+    var cVal C.int
+    if b {
+        cVal = 1
+    }
+    C.lua_pushboolean(s.L, cVal)
+}
+
+func (s *LuaState) PushNil() {
+    C.lua_pushnil(s.L)
+}
---
+++ main.go
@@ -0,0 +1,220 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+
+	// "runtime"
+	"strings"
+
+	"github.com/Breadleaf/LuaBridge/lua"
+)
+
+func main() {
+	// Lock the OS thread to avoid signal issues when mixing Go and C
+	// runtime.LockOSThread()
+
+	L := lua.NewState()
+	defer L.Close()
+
+// 	relaxedRequirePatch := strings.TrimSpace(`
+// -- bootstrap procedure start --
+// _G.__defaultRequire = require
+// _G.require = function( moduleName )
+// 	local module = import( moduleName )
+// 	if type( module ) == "string" and module:find( "not found" ) then
+// 		return _G.__defaultRequire( moduleName )
+// 	else
+// 		return module
+// 	end
+// end
+// --- bootstrap procedure end ---
+// 	`)
+
+	strictRequirePatch := strings.TrimSpace(`
+-- bootstrap procedure start --
+_G.__defaultRequire = require
+_G.require = function(moduleName)
+    local module, err = import(moduleName)
+    if module == nil then
+        if string.find(err or "", "blacklisted") then
+            error(err, 2)
+        end
+        if not string.find(err or "", "blacklisted") then
+            local success, result = pcall(_G.__defaultRequire, moduleName)
+            if success then
+                return result
+            else
+                error(err .. "\n" .. tostring(result), 2)
+            end
+        else
+            error(err, 2)
+        end
+    else
+        return module
+    end
+end
+--- bootstrap procedure end ---
+	`)
+
+	program := strings.TrimSpace(`
+function test()
+	-- causes a stack trace since os was removed
+	print( os.date() )
+end
+
+-- test that black list works and is compliant with either relaxed or strict require patch
+-- NOTE: if using strict require patch, you will need to comment both out as in vanilla lua it
+--       will cause a stack trace and hault execution (this is the behaviour of strict patch)
+-- print( "NOT_REAL_MODULE_OR_IMPORT", require( "NOT_REAL_MODULE_OR_IMPORT" ) )
+-- print( "os", require( "os" ) )
+
+-- test _G.require on default module
+print( require( "math" ) )
+-- local math = require( "math" )
+print( math.sin( 190 ) )
+
+-- test _G.require on custom import
+local mmRequire = require( "mymod" )
+mmRequire.sayHello()
+
+-- test _G.import on custom import
+local mmImport = import( "mymod" )
+mmImport.sayHello()
+
+-- test stack trace w/ program crash
+-- test remove from global scope
+test()
+	`)
+
+	patchProgram := func(source string, patches []string) string {
+		var patchedProgram string
+		for _, patch := range patches {
+			patchedProgram += patch + "\n"
+		}
+		patchedProgram += source
+		return patchedProgram
+	}
+
+	// patchedProgram := patchProgram(program, []string{
+	// 	relaxedRequirePatch,
+	// })
+
+	patchedProgram := patchProgram(program, []string{
+		strictRequirePatch,
+	})
+
+	fmt.Printf(
+		"%s\n%s\n%s\n",
+		"[ PATCHED PROGRAM START ]",
+		patchedProgram,
+		"[  PATCHED PROGRAM END  ]",
+	)
+
+	var imports = map[string]string{
+		"mymod": strings.TrimSpace(`
+local M = {}
+function M.sayHello()
+	print("Hello from mymod!")
+end
+return M
+		`),
+	}
+
+	var blacklist = []string{
+		"os",
+	}
+
+	L.RemoveGlobal("os")
+
+	// relaxed import
+	// L.RegisterGlobalFunction("import", func(L *lua.LuaState) int {
+	// 	importName := L.ToString(1)
+
+	// 	if found := slices.Contains(blacklist, importName); found {
+	// 		L.PushNil()
+	// 		L.PushString(fmt.Sprintf("module '%s' is blacklisted.", importName))
+	// 		return 2
+	// 	}
+
+	// 	source, ok := imports[importName]
+	// 	if !ok {
+	// 		L.PushNil()
+	// 		errorMsg := fmt.Sprintf("module '%s' not found:\n\tno custom import found", importName)
+	// 		L.PushString(errorMsg)
+	// 		return 2
+	// 	}
+
+	// 	if err := L.LoadString(source); err != nil {
+	// 		L.PushNil()
+	// 		L.PushString(err.Error())
+	// 		return 2
+	// 	}
+
+	// 	if err := L.PCall(0, 1, 0); err  != nil {
+	// 		L.PushNil()
+	// 		L.PushString(err.Error())
+	// 		return 2
+	// 	}
+
+	// 	// if import didnt return, push true (default lua module behavior)
+	// 	if L.IsNil(-1) {
+	// 		L.Pop(1)
+	// 		L.PushBoolean(true)
+	// 	}
+
+	// 	return 1
+	// })
+
+	// strict import
+	L.RegisterGlobalFunction("import", func(L *lua.LuaState) int {
+		importName := L.ToString(1)
+
+		if found := slices.Contains(blacklist, importName); found {
+			L.PushNil()
+			errorMsg := fmt.Sprintf("module '%s' not found:\n\tmodule is blacklisted", importName)
+			L.PushString(errorMsg)
+			return 2
+		}
+
+		source, ok := imports[importName]
+		if !ok {
+			L.PushNil()
+			errorMsg := fmt.Sprintf("module '%s' not found:\n\tno field package.preload['%s']\n\tno custom import found",
+				importName, importName)
+			L.PushString(errorMsg)
+			return 2
+		}
+
+		if err := L.LoadString(source); err != nil {
+			L.PushNil()
+			L.PushString(err.Error())
+			return 2
+		}
+
+		if err := L.PCall(0, 1, 0); err != nil {
+			L.PushNil()
+			L.PushString(err.Error())
+			return 2
+		}
+
+		if L.IsNil(-1) {
+			L.Pop(1)
+			L.PushBoolean(true)
+		}
+
+		return 1
+	})
+
+	fmt.Println("RunStringError()")
+	if err := L.RunStringError(patchedProgram); err != nil {
+		fmt.Println(err.Error())
+	}
+
+	/*
+	fmt.Println("RunStringTraceback()")
+	if err := L.RunStringTraceback(patchedProgram); err != nil {
+		fmt.Println(err.Error())
+	}
+	*/
+}
